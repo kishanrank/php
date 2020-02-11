4fece579eb164b32e10db4553255c92e2101da26
kishanrank <kishanrank763@gmail.com>
2020-02-11 11:45:19 +0530

    mvc basic routing

---
+++ 09-02-2020/mvc_1/public/index.php
@@ -0,0 +1,20 @@
+<?php 
+require '../Core/Router.php';
+
+$router = new Router();
+
+$router->add('', ['controller' => 'Home', 'action'=>'index']);
+$router->add('posts', ['controller' => 'Posts', 'action'=> 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action'=> 'new']);
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo "<pre>";
+    var_dump($router->getParams());
+    echo "</pre>";
+} else {
+    echo "No route found for URL '$url'";
+}
+
+?>
