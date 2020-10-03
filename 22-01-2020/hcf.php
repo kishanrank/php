01e493b967eb2ef84d2bd82ebe6d93b20a6ab00a
Yash Prajapati - YPSoft <yashypsoft@gmail.com>
2020-10-03 19:09:07 +0530

    HCF code fix issue

--- 22-01-2020/hcf.php
+++ 22-01-2020/hcf.php
@@ -1,23 +1,40 @@
-<?php 
+<?php
 
-$num1 = 10;
-$num2 = 15;
-$temp;
+function highestCommonFactor($num1, $num2)
+{
 
-if($num1 <$num2) {
-    $temp = $num1;
-}
-for($temp=$num1; $temp>=1; $temp--){
-    if($num1 % $temp == 0 && $num2 % $temp == 0 ) {
-    break;
+    $factor1 = [];
+    $factor2 = [];
+    $commonFactor = [];
+    $j = 0;
+    $k = 0;
+    $x = 0;
+    for ($i = 1; $i <= $num1; $i++) {
+        if ($num1 % $i) {
+        } else {
+            $factor1[$j] = $i;
+            $j++;
+        }
     }
+    for ($i = 1; $i <= $num2; $i++) {
+        if ($num2 % $i) {
+        } else {
+            $factor2[$x] = $i;
+            $x++;
+        }
+    }
+
+    for ($i = 0; $i < sizeof($factor1); $i++) {
+        for ($j = 0; $j < sizeof($factor2); $j++) {
+            if ($factor1[$i] === $factor2[$j]) {
+                $commonFactor[$k] = $factor1[$i];
+                $k++;
+            }
+        }
+    }
+    return $commonFactor[sizeof($commonFactor) - 1];
 }
-echo "HCF of a number " . $temp.'<br>';
 
-
-/////////// LCM /////////////////////////
-
-$result = $num1*$num2/($temp);
-echo "LCM of a number ".$result;
-
-?>
+$val1 = 15;
+$val2 = 10;
+echo  "Highest Common factor for $val1 and $val2 = " . highestCommonFactor($val1, $val2);
